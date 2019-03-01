8dd7757736a6557edd6aef48659445e291b9e413
jjblues86 <jeromejoof@yahoo.com>
2019-02-28 22:49:22 -0800

    completed the requestTime middleware

--- middleware/server.js
+++ middleware/server.js
@@ -5,18 +5,29 @@
  */
 const express = require('express');
 const errorMiddleware = require('./error');
+const moment = require('moment');
 
 const app = express();
 
 // Jerome - set up a middleware before any route is defined.
 // Jerome - Having an empty app.use will always result to a Global middleware
 
-app.use();
+// Jerome - Write middleware that runs on every route that adds a property called requestTime with a value of the current Date/Time to the request object.
+
+const requestTime = (req, res, next) => {
+    req.requestTime = Date.now();
+     console.log(`${req.requestTime}`);
+    next();
+};
+
+app.use(requestTime);
 
 
-const app = express();
+
 
 const PORT = process.env.PORT || 8080;
+
+
 
 app.get('/a', (req,res) => {
     res.status(200).send('Route A');
